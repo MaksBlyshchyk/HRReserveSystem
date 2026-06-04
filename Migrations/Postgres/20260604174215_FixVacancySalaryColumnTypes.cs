3949551a4a7c75b47a27fc7712fb251c1b699cfd
MaksBlyshchyk <blyshchyk_ak24@nuwm.edu.ua>
2026-06-04 20:46:57 +0300

    Reset migrations to create clean numeric columns

---
+++ Migrations/Postgres/20260604174215_FixVacancySalaryColumnTypes.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HRReserveSystem.Migrations.Postgres
+{
+    /// <inheritdoc />
+    public partial class FixVacancySalaryColumnTypes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
