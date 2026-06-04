07940a052e0ccad75cf45e1c02fb199b062284ab
MaksBlyshchyk <blyshchyk_ak24@nuwm.edu.ua>
2026-06-04 19:43:31 +0300

    Fix Salary columns type for PostgreSQL

--- Migrations/Postgres/20260604164208_FixVacancySalaryColumnTypes.Designer.cs
+++ Migrations/Postgres/20260604164208_FixVacancySalaryColumnTypes.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using HRReserveSystem.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace HRReserveSystem.Migrations.Postgres
 {
     [DbContext(typeof(PostgresApplicationDbContext))]
-    partial class PostgresApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260604164208_FixVacancySalaryColumnTypes")]
+    partial class FixVacancySalaryColumnTypes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
